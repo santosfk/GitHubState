1fc8657ef641b18e39289b7550320baa9139da73
Guilherme Santos <santosguilhermefk@gmail.com>
2022-02-19 15:56:45 -0300

    add 'MyPacks' route

---
+++ src/Pages/myPacks/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function MyPacks() {
+  return <h1>my packs here</h1>;
+}
+
+export default MyPacks;
--- src/Routes/Routes.tsx
+++ src/Routes/Routes.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import { Routes, Route, Link } from "react-router-dom";
 import Home from "../Pages/Home";
 import Me from "../Pages/Me";
+import MyPacks from "../Pages/myPacks";
 
 function RoutesFunc() {
   return (
     <Routes>
       <Route path="" element={<Home />} />
       <Route path="me" element={<Me />} />
+      <Route path="me" element={<MyPacks />} />
     </Routes>
   );
 }
