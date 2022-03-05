ff2702cd95ef80934bd817044f066126a785a145
santosfk <santosguilhermefk@gmail.com>
2022-03-04 21:35:48 -0300

    creating style of login form

--- src/Pages/Me/index.tsx
+++ src/Pages/Me/index.tsx
@@ -3,7 +3,9 @@ import client from "../../services/client";
 import LoginModal from "../../components/LoginModal/LoginModal";
 import * as style from "./style";
 function Me() {
-  const [userReceive, setUserReceive] = useState<Object>({});
+  const [userReceive, setUserReceive] = useState<React.SetStateAction<Object>>(
+    {}
+  );
   const [modalOn, setModalOn] = useState(false);
   const userRef = useRef<HTMLInputElement>(null);
   const nameRef = useRef<HTMLInputElement>(null);
@@ -20,22 +22,25 @@ function Me() {
   console.log(modalOn);
 
   return (
-    <>
+    <style.Container>
       {modalOn && <LoginModal response={Response} />}
-      <h1>Make Your Login </h1>
-      <input
-        type="text"
-        placeholder="digite seu usuário no github"
-        ref={userRef}
-      />
-      <input
-        type="text"
-        placeholder="digite seu nome no github"
-        ref={nameRef}
-      />
-
-      <button onClick={Logar}> logar</button>
-    </>
+      <style.Content>
+        <style.Title>Faça Seu Login </style.Title>
+        <style.InputContent>
+          <style.User
+            type="text"
+            placeholder="digite seu usuário no github"
+            ref={userRef}
+          />
+          <style.Name
+            type="text"
+            placeholder="digite seu nome no github"
+            ref={nameRef}
+          />
+        </style.InputContent>
+        <button onClick={Logar}> logar</button>
+      </style.Content>
+    </style.Container>
   );
 }
 
--- src/Pages/Me/style.ts
+++ src/Pages/Me/style.ts
@@ -1,8 +1,76 @@
 import styled from "styled-components";
-
-export const LoginModal = styled.div`
-  position: absolute;
-  width: 800px;
-  height: 800px;
-  background-color: black;
+export const Content = styled.div`
+  @import url("https://fonts.googleapis.com/css2?family=Rubik:ital,wght@1,300&display=swap");
+  background-color: white;
+  box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+  height: 550px;
+  width: 600px;
+  margin: auto;
+  margin-top: 5rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  button {
+    height: 50px;
+    margin-top: 2rem;
+    width: 80%;
+    border: none;
+    background: linear-gradient(
+      90deg,
+      rgba(2, 0, 36, 1) 0%,
+      rgba(31, 194, 2, 1) 0%,
+      rgba(20, 181, 154, 1) 60%,
+      rgba(74, 147, 236, 1) 100%
+    );
+    color: white;
+    font-size: 2rem;
+    letter-spacing: 3px;
+    transition: 0.5s;
+  }
+  button:hover {
+    background: #41d773;
+    border: 1px solid #41d773;
+    color: white;
+  }
+`;
+export const Title = styled.h1`
+  font-family: "Rubik", sans-serif;
+  text-align: center;
+  color: #41d773;
+  font-size: 3rem;
+`;
+export const InputContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 2rem;
+  margin-bottom: 4rem;
+  input {
+    border: none;
+    outline: none;
+    background-color: #41d773;
+    width: 500px;
+    color: white;
+    padding: 5px;
+    font-size: 1rem;
+    border-radius: 15px;
+    box-shadow: rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px;
+    transition: 0.5s;
+  }
+  input:focus {
+    border-radius: 25px;
+  }
+  input::placeholder {
+    color: white;
+  }
+`;
+export const User = styled.input`
+  height: 50px;
+  margin-bottom: 2rem;
+`;
+export const Name = styled.input`
+  height: 50px;
+`;
+export const Container = styled.div`
+  width: 100%;
+  height: 100%;
 `;
