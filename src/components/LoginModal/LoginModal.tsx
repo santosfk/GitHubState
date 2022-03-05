1fc384c1837255892dfc1ef652a12bc29869e81e
santosfk <santosguilhermefk@gmail.com>
2022-03-05 18:40:04 -0300

    adding data to the modal

--- src/components/LoginModal/LoginModal.tsx
+++ src/components/LoginModal/LoginModal.tsx
@@ -7,8 +7,26 @@ function LoginModal({ userReceive }: Props) {
   return (
     <style.Container>
       <style.Img src={userReceive.avatar_url} />
-      <style.Name>{userReceive.name}</style.Name>,
+      <style.Name>{userReceive.name}</style.Name>
       <style.Login>{userReceive.login}</style.Login>
+      <style.Infos>
+        <style.Id>
+          <h3>Id </h3> <h2>{userReceive.id}</h2>
+        </style.Id>
+        <style.Repos>
+          <h3> Repos</h3> <h2>{userReceive.public_repos}</h2>
+        </style.Repos>
+      </style.Infos>
+      <style.Branding>
+        <style.Followers>
+          <h3> Followers </h3>
+          <h2> {userReceive.followers}</h2>
+        </style.Followers>
+        <style.Following>
+          <h3>Following</h3> <h2> {userReceive.following}</h2>
+        </style.Following>
+      </style.Branding>
+      <style.Bio>{userReceive.bio}</style.Bio>
     </style.Container>
   );
 }
--- src/components/LoginModal/style.ts
+++ src/components/LoginModal/style.ts
@@ -1,13 +1,74 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
+  @import url("https://fonts.googleapis.com/css2?family=Smooch+Sans&display=swap");
+  font-family: "Smooch Sans", sans-serif;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   position: absolute;
   width: 100%;
   height: 100vh;
   background-color: black;
   top: 0;
   color: white;
+  h3 {
+    color: #41d773;
+    margin-bottom: -2px;
+    margin: auto;
+    font-size: 2rem;
+    transition: 0.4s;
+  }
+  h3:hover {
+    color: #12e7c4;
+  }
+  h2 {
+    text-align: center;
+    margin-top: -3px;
+  }
 `;
-export const Img = styled.img``;
+export const Img = styled.img`
+  border-radius: 50%;
+  width: 200px;
+  margin-top: 50px;
+  border: solid 2px #41d773;
+`;
 export const Name = styled.h1``;
-export const Login = styled.h2``;
+export const Login = styled.h2`
+  margin-top: -10px;
+  color: #41d773;
+`;
+export const Infos = styled.div`
+  display: flex;
+  align-items: center;
+  margin-top: 15px;
+`;
+export const Id = styled.h2`
+  margin-right: 200px;
+  text-align: center;
+`;
+export const Repos = styled.div`
+  margin-left: 200px;
+  font-size: 2rem;
+`;
+
+export const Branding = styled.div`
+  display: flex;
+  align-items: center;
+  align-items: center;
+  margin-top: 30px;
+`;
+export const Followers = styled.div`
+  font-size: 2rem;
+  margin-right: 23rem;
+`;
+
+export const Following = styled.div`
+  font-size: 2rem;
+  margin-left: 2rem;
+`;
+export const Bio = styled.div`
+  margin-top: 20px;
+  font-size: 2rem;
+  color: #41d773;
+`;
